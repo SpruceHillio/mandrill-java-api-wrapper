dbbb80c6c03dcdb02dab8db36a81124e745aa6b0
Stephan Wienczny <wienczny@gmail.com>
2014-08-07 13:21:23 +0200

    Update Tests

--- src/test/java/io/sprucehill/mandrill/service/TemplateServiceTest.java
+++ src/test/java/io/sprucehill/mandrill/service/TemplateServiceTest.java
@@ -1,6 +1,7 @@
 package io.sprucehill.mandrill.service;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
 
 import org.junit.Assert;
@@ -9,33 +10,49 @@ import org.junit.Test;
 
 import io.sprucehill.mandrill.AbstractTest;
 import io.sprucehill.mandrill.data.error.PreBuildError;
+import io.sprucehill.mandrill.data.error.RenderTemplateError;
 import io.sprucehill.mandrill.data.request.TemplateAddPayload;
+import io.sprucehill.mandrill.data.request.TemplateDeletePayload;
 import io.sprucehill.mandrill.data.request.TemplateRenderPayload;
 import io.sprucehill.mandrill.data.request.TemplateUpdatePayload;
 import io.sprucehill.mandrill.data.response.TemplateResponse;
 
 public class TemplateServiceTest extends AbstractTest {
-    private static final String TEMPLATE = "testtemplate";
+    private static final String TEMPLATE = "testtemplate" + System.currentTimeMillis();
 
-    @Ignore
     @Test
     public void add() throws PreBuildError, IOException {
         final TemplateService messageService = new TemplateService();
 
-        final TemplateResponse response = messageService.add(
-                new TemplateAddPayload.Builder().withName(TEMPLATE));
+        try {
+            final TemplateResponse response = messageService.add(
+                    new TemplateAddPayload.Builder().withName(TEMPLATE));
 
-        Assert.assertEquals(TEMPLATE, response.getName());
+            Assert.assertEquals(TEMPLATE, response.getName());
+        } finally {
+            delete();
+        }
+
     }
 
     @Test
     public void update() throws PreBuildError, IOException {
         final TemplateService messageService = new TemplateService();
 
-        final TemplateResponse response = messageService.update(
-                new TemplateUpdatePayload.Builder().withName(TEMPLATE));
+        try {
+            try {
+                messageService.add(new TemplateAddPayload.Builder().withName(TEMPLATE));
+            } catch (final RenderTemplateError e) {
+                // Ignore
+            }
 
-        Assert.assertEquals(TEMPLATE, response.getName());
+            final TemplateResponse response = messageService.update(
+                    new TemplateUpdatePayload.Builder().withName(TEMPLATE));
+
+            Assert.assertEquals(TEMPLATE, response.getName());
+        } finally {
+            delete();
+        }
     }
 
 
@@ -43,8 +60,33 @@ public class TemplateServiceTest extends AbstractTest {
     public void render() throws PreBuildError, IOException {
         final TemplateService messageService = new TemplateService();
 
-        final String response = messageService.render(
-                new TemplateRenderPayload.Builder().withTemplateName(TEMPLATE));
+        try {
+            try {
+                messageService.add(new TemplateAddPayload.Builder().withName(TEMPLATE));
+            } catch (final RenderTemplateError e) {
+                // Ignore
+            }
+
+            final String response = messageService.render(
+                    new TemplateRenderPayload.Builder().withTemplateName(TEMPLATE));
+        } finally {
+            delete();
+        }
+
+        //Assert.assertFalse(response.isEmpty());
+        //Assert.assertEquals(EMAIL, response.getEmail());
+        //Assert.assertEquals(MessageResponse.Status.SENT, response.getStatus());
+    }
+
+    public void delete() throws PreBuildError, IOException {
+        final TemplateService messageService = new TemplateService();
+
+        try {
+            final TemplateResponse response = messageService.delete(
+                    new TemplateDeletePayload.Builder().withName(TEMPLATE));
+        } catch (RenderTemplateError ex) {
+            // Ignore
+        }
 
         //Assert.assertFalse(response.isEmpty());
         //Assert.assertEquals(EMAIL, response.getEmail());
