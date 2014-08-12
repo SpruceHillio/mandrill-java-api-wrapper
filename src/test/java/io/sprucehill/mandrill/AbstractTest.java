f31e99603540fab3c0b5e69b3c941ef6c6daa0be
Stephan Wienczny <wienczny@gmail.com>
2014-08-12 11:14:54 +0200

    Fix apiKey resolving. Properties were missing

--- src/test/java/io/sprucehill/mandrill/AbstractTest.java
+++ src/test/java/io/sprucehill/mandrill/AbstractTest.java
@@ -5,25 +5,35 @@ import org.junit.Before;
 import io.sprucehill.mandrill.service.AbstractService;
 
 /**
- * Created by stephan on 06.08.14.
+ * Abstract test providing apiKey.
  */
 public abstract class AbstractTest {
-    protected String apikey = null;
+    protected String apiKey = null;
 
     @Before
     public void init() {
         // 1. try
-        apikey = System.getenv("mandrill.apikey");
+        apiKey = System.getenv("mandrill.apikey");
 
         // 2. try
-        if (apikey == null) {
-            apikey = System.getenv("mandrill_apikey");
+        if (apiKey == null || apiKey.isEmpty()) {
+            apiKey = System.getenv("mandrill_apikey");
+        }
+
+        // 3. try
+        if (apiKey == null || apiKey.isEmpty()) {
+            apiKey = System.getProperty("mandrill.apikey");
+        }
+
+        // 4. try
+        if (apiKey == null || apiKey.isEmpty()) {
+            apiKey = System.getProperty("mandrill_apikey");
         }
 
         // failed?
-        if (apikey == null) {
-            throw new IllegalArgumentException("No apikey in environment");
+        if (apiKey == null) {
+            throw new IllegalArgumentException("No apiKey in environment");
         }
-        AbstractService.setKey(apikey);
+        AbstractService.setKey(apiKey);
     }
 }
